5d33ba2c7f1f83ce9bcd70c889053e5f19b184cc
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-09-11 23:21:01 -0400

    Added empty game objects above and below the player to attach the camera to

--- ThirdTryUnity/Assets/CameraFollowOptionTwo.cs
+++ ThirdTryUnity/Assets/CameraFollowOptionTwo.cs
@@ -5,10 +5,12 @@ using System;
 
 public class CameraFollowOptionTwo : MonoBehaviour
 {
-
-    public Transform _target;
-    public float _speed = 1.0f;
+    public Rigidbody2D _player;
+    private Transform _camerasFocusPoint;
+    public Transform _cameraMovingUpTarget;
+    public Transform _cameraMovingDownTarget;
     public Transform _endPoint;
+    public float _speed = 1.0f;
     public float _yMin;
     public float _yMax;
     private Vector3 _v3;
@@ -17,63 +19,46 @@ public class CameraFollowOptionTwo : MonoBehaviour
     private float _differenceBetweenPoints;
     private float _startingEndPointYValue;
     private float _nextEndPosition;
-    private float _currentOne;
-    private float _nextOne;
+    private float _currentEndPointPosition;
     public float[] _endPointYs;
+
     private void Start()
     {
+        _camerasFocusPoint = _cameraMovingUpTarget;
         _playerBounce = FindObjectOfType<PlayerBounce>();
         _v3 = transform.position;
         _startingEndPointYValue = Math.Abs(_endPoint.position.y);
-        _differenceBetweenPoints = (Math.Abs(_endPoint.position.y) - Math.Abs(_target.position.y));
-        _currentOne = _endPoint.position.y;
+        _differenceBetweenPoints = (Math.Abs(_endPoint.position.y) - Math.Abs(_camerasFocusPoint.position.y));
+        _currentEndPointPosition = _endPoint.position.y;
     }
 
     private void FixedUpdate()
     {
-        //if(_playerBounce._bounceCount < 3)
-        //{
-        //    _differenceBetweenPoints = (_startingEndPointYValue - Math.Abs(_target.position.y));
-        //    _currentOne = _playerBounce._nextEndPointPosition;
-        //}
-        //else
-        //{
-        //    _differenceBetweenPoints = (_playerBounce._nextEndPointPosition - Math.Abs(_target.position.y));
-        //    _currentOne = _playerBounce._nextEndPointPosition;
-        //}          
-        if (_playerBounce._bounceCount  <= 1)
+
+        //Determining if the players rigidbody is moving up or down. A negative Y velocity means moving down, positive Y velocity means moving up.
+        if (_player.velocity.y > 0)
         {
-            _differenceBetweenPoints = (_startingEndPointYValue - Math.Abs(_target.position.y));
-            _currentOne = _playerBounce._nextEndPointPosition;
+            _camerasFocusPoint = _cameraMovingUpTarget;
         }
         else
         {
-            Debug.Log(_playerBounce._bounceCount);
-        Debug.Log(_endPointYs[_playerBounce._bounceCount - 2]);
-                _differenceBetweenPoints = (_endPointYs[_playerBounce._bounceCount - 2] - Math.Abs(_target.position.y));
-                _currentOne = _playerBounce._nextEndPointPosition;
-       }
-        //else if (_playerBounce._bounceCount == 3)
-        //{
-        //    Debug.Log(_playerBounce._currentEndPointPosition);
-        //    _differenceBetweenPoints = (13 - Math.Abs(_target.position.y));
-        //    _currentOne = _playerBounce._nextEndPointPosition;
-        //}
-        //else if (_playerBounce._bounceCount == 4)
-        //{
-        //    _differenceBetweenPoints = (21 - Math.Abs(_target.position.y));
-        //    _currentOne = _playerBounce._nextEndPointPosition;
-        //}
-        //else if (_playerBounce._bounceCount == 5)
-        //{
-        //    _differenceBetweenPoints = (29 - Math.Abs(_target.position.y));
-        //    _currentOne = _playerBounce._nextEndPointPosition;
-        //}
+            _camerasFocusPoint = _cameraMovingDownTarget;
+        }
 
-    }
-
-    void LateUpdate()
-    {
+        //Calculating the difference between the player and the end point based on the bounce count and the endPoints Y value.
+        if (_playerBounce._bounceCount <= 1)
+        {
+            _differenceBetweenPoints = (_startingEndPointYValue - Math.Abs(_camerasFocusPoint.position.y));
+            _currentEndPointPosition = _playerBounce._nextEndPointPosition;
+        }
+        else
+        {
+            _differenceBetweenPoints = (_endPointYs[_playerBounce._bounceCount - 2] - Math.Abs(_camerasFocusPoint.position.y));
+            _currentEndPointPosition = _playerBounce._nextEndPointPosition;
+        }
+        
+     
+        //Determining the difference between the player and the end point to stop or start the camera
         if (_distanceCameraStopsFromTop >= _differenceBetweenPoints)
         {
             _speed = 0f;
@@ -84,12 +69,12 @@ public class CameraFollowOptionTwo : MonoBehaviour
             _speed = 1.0f;
             CameraLerp();
         }
-
     }
+
     private void CameraLerp()
     {
         Vector3 v3 = transform.position;
-        v3.y = Mathf.Lerp(v3.y, _target.position.y, _speed);
+        v3.y = Mathf.Lerp(v3.y, _camerasFocusPoint.position.y, _speed);
         transform.position = new Vector3(v3.x, Mathf.Clamp(v3.y, _yMin, _yMax), v3.z);
     }
 
