e54076a36a77beb7f1705b1f6e78f42c63714a4c
tylerseawell4 <tylerseawell4@gmail.com>
2017-09-15 19:27:21 -0400

    added gravity script

---
+++ ThirdTryUnity/Assets/Scripts/GravityPlayerBounce.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPlayerBounce : MonoBehaviour
+{
+
+    public Rigidbody2D _player;
+    public float _startingHeight;
+    private float _maxSpeed;
+    private bool _shouldIncreaseGravity;
+    public float _startingGravity;
+    public float _increaseHeightBy;
+    private float _a;
+    public float _bounceCount;
+    private bool _shouldLog;
+    public float _maxGravityValue;
+    // Use this for initialization
+    void Start()
+    {
+        _player.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        _maxSpeed = _startingHeight / 4;
+        _a = (_startingHeight / 10f) * .001f;
+        _shouldLog = true;
+        _bounceCount = 1;
+        _maxGravityValue = _bounceCount / 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_player.velocity.magnitude >= _maxSpeed)
+        {
+            _shouldIncreaseGravity = true;
+        }
+        else
+        {
+            _shouldIncreaseGravity = false;
+        }
+
+        if (_shouldIncreaseGravity && _player.gravityScale <= _maxGravityValue)
+        {
+            _player.gravityScale += (.01f);
+        }
+        else if (_player.gravityScale >= _maxGravityValue)
+        {
+
+            if (_shouldLog)
+            {
+                Debug.Log(_player.transform.position.y);
+                _shouldLog = false;
+            }
+               
+        }
+        //Debug.Log(_player.velocity.magnitude);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _bounceCount++;
+        _player.gravityScale = -10f;
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _startingHeight += _increaseHeightBy;
+        _maxSpeed = _startingHeight / 4;
+        _player.gravityScale = _startingGravity;
+        _shouldLog = true;
+        _maxGravityValue = _bounceCount/10;
+        //_a = (_maxHeight / 10f) * .001f ;
+    }
+}
