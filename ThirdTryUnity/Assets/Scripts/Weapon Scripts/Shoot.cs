3aaf06337836cc1899a904046bfa342a9134dd8b
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-10-13 21:54:17 -0400

    Added a firing rate on the Shoot script to limit the number of shots fired in a given time frame.

--- ThirdTryUnity/Assets/Scripts/Weapon Scripts/Shoot.cs
+++ ThirdTryUnity/Assets/Scripts/Weapon Scripts/Shoot.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Shoot : MonoBehaviour {
     [Tooltip("The 'bullet' that will shoot out of firepoint. The bullet Asset MUST have the MoveAndDestroyWeapon script attached to it.")]
     public GameObject _bullet;
+    [Tooltip("How many 'bullets' can fire per second")]
+    public float _fireRate;
 
-    private float _timeToSpawnEffect;
+    private float _nextTimeToFire;
     private Transform _firePointTransform;
     private PlayerControl _playerControl;
 
@@ -19,16 +21,12 @@ public class Shoot : MonoBehaviour {
 	
 
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= _nextTimeToFire)
         {
-            ShootBullet();
+            _nextTimeToFire = Time.time + 1f / _fireRate;
+            Fire();
         }
-    }
-    private void ShootBullet()
-    {
-        Fire();
-    }
-
+    }    
     private void Fire()
     {
         //calling the generic get pooled object class to obtain game objects instead of instantiating them here
