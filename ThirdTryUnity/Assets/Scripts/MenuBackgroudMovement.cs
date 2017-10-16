0b7723cb0e31eca2d257a7bbd31f617240eb7221
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-10-16 00:25:51 -0400

    Added a main menu scene, and three new scripts associated with the menu

---
+++ ThirdTryUnity/Assets/Scripts/MenuBackgroudMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackgroudMovement : MonoBehaviour {
+    
+    public float _speed = .25f;
+    public Transform _leftOutterBounds;
+    public Transform _rightOutterBounds;
+
+    private float _cloudYPosition;
+
+    private void Start()
+    {
+        _cloudYPosition = transform.position.y;       
+    }
+
+    void Update () {
+        transform.Translate(Vector3.right * Time.deltaTime * _speed);        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "RightCamera")
+        {
+            transform.position = new Vector3(_leftOutterBounds.transform.position.x, _cloudYPosition, transform.position.z);
+            Debug.Log(_leftOutterBounds.transform.position.x);
+        } 
+    }
+}
