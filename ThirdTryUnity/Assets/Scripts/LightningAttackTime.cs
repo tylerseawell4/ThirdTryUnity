5d13cab446e526ed9327708c2ea017ee391460f6
tylerseawell4 <tylerseawell4@gmail.com>
2017-11-09 22:38:46 -0500

    changed the scale of the lightening to match the exact length/distance to the enemy when shooting it

--- ThirdTryUnity/Assets/Scripts/LightningAttackTime.cs
+++ ThirdTryUnity/Assets/Scripts/LightningAttackTime.cs
@@ -21,8 +21,13 @@ public class LightningAttackTime : MonoBehaviour
     {
         if (_enemy != null)
         {
+
             transform.up = _enemy.position - transform.position;
-           // transform.rotation = Quaternion.Euler(0, 0, -90);
+            // transform.rotation = Quaternion.Euler(0, 0, -90);
+            var distance = Vector3.Distance(transform.position, _enemy.position);
+            var yScale = transform.localScale.y;
+            yScale = distance / 4;
+            transform.localScale = new Vector2(transform.localScale.x, yScale);
         }
 
         _lightningAcumTime -= 1f * Time.deltaTime;
