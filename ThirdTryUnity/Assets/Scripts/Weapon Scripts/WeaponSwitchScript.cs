f6f09e9c307498268362d36dc1cfbd63746ee81f
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-11-30 22:05:39 -0500

    Added two new scripts for the ice shoot and ice shotgun, modified the weapon switch script, and added the ice scripts to the player game object.

--- ThirdTryUnity/Assets/Scripts/Weapon Scripts/WeaponSwitchScript.cs
+++ ThirdTryUnity/Assets/Scripts/Weapon Scripts/WeaponSwitchScript.cs
@@ -10,6 +10,8 @@ public class WeaponSwitchScript : MonoBehaviour
     private KetchupShotgun _ketchupShotgun;
     private MustardShoot _mustardShoot;
     private MustardShotgun _mustardShotgun;
+    private IceShoot _iceShoot;
+    private IceShotgun _iceShotgun;
     private TapManager _tapManager;
     private void Start()
     {
@@ -18,11 +20,15 @@ public class WeaponSwitchScript : MonoBehaviour
         _ketchupShotgun = FindObjectOfType<KetchupShotgun>();
         _mustardShoot = FindObjectOfType<MustardShoot>();
         _mustardShotgun = FindObjectOfType<MustardShotgun>();
+        _iceShoot = FindObjectOfType<IceShoot>();
+        _iceShotgun = FindObjectOfType<IceShotgun>();
 
         if (_ketchupShotgun.enabled && _ketchupShotgun.enabled)
             _switchBtnText.text = "Ketchup";
-        else
+        else if (_mustardShoot.enabled && _mustardShotgun.enabled)
             _switchBtnText.text = "Mustard";
+        else if (_iceShoot.enabled && _iceShotgun.enabled)
+            _switchBtnText.text = "Ice";
     }
 
     public void SwitchWeapon()
@@ -36,16 +42,22 @@ public class WeaponSwitchScript : MonoBehaviour
             _ketchupShotgun.enabled = false;
             _mustardShoot.enabled = true;
             _mustardShotgun.enabled = true;
-
         }
-        else
+        else if (_mustardShoot.enabled && _mustardShotgun.enabled)
         {
-            _switchBtnText.text = "Ketchup";
+            _switchBtnText.text = "Ice";
             _mustardShoot.enabled = false;
             _mustardShotgun.enabled = false;
+            _iceShoot.enabled = true;
+            _iceShotgun.enabled = true;
+        }
+        else if (_iceShoot.enabled && _iceShotgun.enabled)
+        {
+            _switchBtnText.text = "Ketchup";
+            _iceShotgun.enabled = false;
+            _iceShoot.enabled = false;
             _ketchupShoot.enabled = true;
             _ketchupShotgun.enabled = true;
-
         }
     }
 }
