fbb78fe3b38c70e82fb433c5161cbe20fc9aae7a
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-12-10 21:39:40 -0500

    Fixed the bug in the script that was downloaded from the asset store

--- ThirdTryUnity/Assets/Dungeon Art Pack/AnimateSprite.cs
+++ ThirdTryUnity/Assets/Dungeon Art Pack/AnimateSprite.cs
@@ -42,7 +42,7 @@ public class AnimateSprite : MonoBehaviour
 	    float offsetY = (1.0f - size.y) - (vIndex + rowNumber) * size.y;
 	    Vector2 offset = new Vector2(offsetX,offsetY);
 	 
-	    renderer.material.SetTextureOffset ("_MainTex", offset);
-	    renderer.material.SetTextureScale  ("_MainTex", size);
+        GetComponent<Renderer>().material.SetTextureOffset ("_MainTex", offset);
+        GetComponent<Renderer>().material.SetTextureScale  ("_MainTex", size);
 	}
 }
