751f046cd364322663b1cdd9586d566fe394c55d
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-12-13 17:00:35 -0500

    Checking in the inventory scene

--- ThirdTryUnity/Assets/Scripts/Inventory/Orientation.cs
+++ ThirdTryUnity/Assets/Scripts/Inventory/Orientation.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Orientation : MonoBehaviour {
+    Scene _scene;
+    // Use this for initialization
+    void Start () {
+        _scene = SceneManager.GetActiveScene();
+        if (_scene.buildIndex == 3)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+        else
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+        }
 
-	// Use this for initialization
-	void Start () {
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update () {
+     //   Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
 }
