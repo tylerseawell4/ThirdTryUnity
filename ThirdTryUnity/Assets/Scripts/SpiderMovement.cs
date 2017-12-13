63929aa19c621958afeea5159e1587988211631e
tylerseawell4 <tylerseawell4@gmail.com>
2017-12-13 18:16:10 -0500

    added spider movement on the web

---
+++ ThirdTryUnity/Assets/Scripts/SpiderMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour
+{
+    private float _rotateSpeed = 1.5f;
+    private float _radius = 2.1f;
+    private Vector2 _centre;
+    private float _angle;
+
+    private void Start()
+    {
+        _centre = transform.position;
+    }
+
+    private void Update()
+    {
+        _angle += _rotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * _radius;
+        transform.position = _centre + offset;
+
+    }
+}
