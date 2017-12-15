764e3ce3ccd1bf71a2c20187e13313623ac7aa46
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2017-12-14 23:11:04 -0500

    Added the scroll capability to the inventory scroll list, and added a couple scripts for the menu

---
+++ ThirdTryUnity/Assets/Scripts/Inventory/ScrollableList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollableList : MonoBehaviour {
+   
+    private GameObject _scrollablePanel;
+    private ScrollRect _scroll;
+
+    public void Start()
+    {
+        _scrollablePanel = GameObject.Find("ScrollableListPanel");
+        _scroll = _scrollablePanel.GetComponent<ScrollRect>();
+    }
+    private void Update()
+    {
+      
+    }
+    public void MoveScrollBar(string button)
+    {
+       
+        if (button == "right")
+        {
+           
+        }
+        else if (button =="left")
+        {
+            
+        }
+    }
+}
