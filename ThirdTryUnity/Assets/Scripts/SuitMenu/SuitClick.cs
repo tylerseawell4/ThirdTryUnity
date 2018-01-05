663fa57f8213e0ae9d58445ca25dbc1be6d97ac8
emkaybeiam <31328898+emkaybeiam@users.noreply.github.com>
2018-01-04 23:11:01 -0500

    Added a Suit menu, just playing around with some UI ideas. Using the planet art as placeholders

---
+++ ThirdTryUnity/Assets/Scripts/SuitMenu/SuitClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuitClick : MonoBehaviour {
+    private GameObject _doojee;
+    private SpriteRenderer _doojeeSprite;
+    private Text _suitName;
+    private Image _gameObjectImage;
+
+    public void AnimateDoojee()
+    {
+        _suitName = GameObject.Find("SuitName").GetComponent<Text>();
+        if (_suitName != null)
+        {
+            _suitName.text = gameObject.name;
+        }
+
+
+        _doojee = GameObject.Find("Body");
+        if (_doojee != null)
+        {
+            _doojeeSprite = _doojee.GetComponent<SpriteRenderer>();
+            _gameObjectImage = gameObject.GetComponent<Image>();
+            _doojeeSprite.sprite = _gameObjectImage.sprite;
+        }
+    }
+}
