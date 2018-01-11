11d768f51fc0dfb73fe7000a28dc9360900301f9
tylerseawell4 <tylerseawell4@gmail.com>
2018-01-11 18:22:46 -0500

    added an ad manager script and added 2 buttons that tie in the ads to button clicks

---
+++ ThirdTryUnity/Assets/Scripts/Ads/AdManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+[RequireComponent(typeof(Button))]
+public class AdManager : MonoBehaviour
+{
+    Button m_Button;
+    public string placementId = "rewardedVideo";
+
+#if UNITY_IOS
+   private string    _gameID = "1666894";
+#elif UNITY_ANDROID
+    private string _gameID = "1666893";
+#elif UNITY_EDITOR
+    private string _gameID = "1666893";
+#endif
+
+    void Awake()
+    {
+        m_Button = GetComponent<Button>();
+        if (m_Button) m_Button.onClick.AddListener(ShowAd);
+
+        if (Advertisement.isSupported)
+            Advertisement.Initialize(_gameID, true);
+    }
+
+    public void ShowAd()
+    {
+#if UNITY_EDITOR
+        StartCoroutine(WaitForAd());
+#endif
+
+        ShowOptions options = new ShowOptions();
+        options.resultCallback = AdCallbackhandler;
+
+        Advertisement.Show(placementId, options);
+    }
+
+    private void Update()
+    {
+        if (m_Button)
+            m_Button.interactable = Advertisement.IsReady(placementId);
+    }
+
+    void AdCallbackhandler(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("Ad Finished. Rewarding player...");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Ad skipped. Son, I am dissapointed in you");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("I swear this has never happened to me before");
+                break;
+        }
+    }
+
+    IEnumerator WaitForAd()
+    {
+        float currentTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
+        yield return null;
+
+        while (Advertisement.isShowing)
+            yield return null;
+
+        Time.timeScale = currentTimeScale;
+    }
+}
